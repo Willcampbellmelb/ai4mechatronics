5166d1aaa76085cbfbcf4d53790b950a9faa7206
William Campbell <wacampbel@gmail.com>
2019-04-02 16:07:14 +1100

    Create bonus.py

---
+++ project_01/bonus.py
@@ -0,0 +1,8 @@
+#SAME JUPYTER NOTEBOOK
+# explain which you have done (DO 1 min), code and results,(please also provide saved files and/or Matplotlib code to generate figures, if applicable)
+# Answer: does everything work as expected? explain why or why not
+
+#Do.
+# Compare 2 or 3 gradient methods or learning rate schedules
+# Do ablation study on 2 or 3 regularization methods.
+# Compare 2 or 3 network architecturtes.
