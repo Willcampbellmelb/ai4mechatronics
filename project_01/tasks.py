21f9b025c50c46cea3c945a0ef53de17eaaa4c6d
William Campbell <wacampbel@gmail.com>
2019-04-02 16:05:57 +1100

    Create tasks.py

---
+++ project_01/tasks.py
@@ -0,0 +1,4 @@
+#  Jupyter Notebook w/ steps for re-running experiments (classes + functions all in .py files)
+#  Saved ckpt files for one of models and accompanying code from above jup so tests can be re run
+#  Saved event files (for @ least 1 of the models)-> for evaluation in TensorBoard
+#  Metadata files needed for visualization of model in TensorBoard and chrome://tracing
